0b037a502a33451c4a8e06148937dadad048f4f4
Gr3n0xX <78820403+grenoxx@users.noreply.github.com>
2021-03-21 18:16:21 -0300

    Add files via upload

---
+++ My/MyComputer.cs
@@ -0,0 +1,21 @@
+﻿//Type: Gr3eNoX_Exploit_Scanner_V80._0.My.MyProject
+// Assembly: Gr3eNoX Exploit Scanner V8.0, Version=8.0.0.0, Culture=neutral, PublicKeyToken=null
+
+using Microsoft.VisualBasic.Devices;
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace Gr3eNoX_Exploit_Scanner_V80._0.My
+{
+  [EditorBrowsable(EditorBrowsableState.Never)]
+  [GeneratedCode("MyTemplate", "10.0.0.0")]
+  internal class MyComputer : Computer
+  {
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    [DebuggerHidden]
+    public MyComputer()
+    {
+    }
+  }
+}
---
+++ My/MySettings.cs
@@ -0,0 +1,67 @@
+﻿//Type: Gr3eNoX_Exploit_Scanner_V80._0.My.MyProject
+// Assembly: Gr3eNoX Exploit Scanner V8.0, Version=8.0.0.0, Culture=neutral, PublicKeyToken=null
+
+using Microsoft.VisualBasic.ApplicationServices;
+using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Configuration;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace Gr3eNoX_Exploit_Scanner_V80._0.My
+{
+  [GeneratedCode("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "12.0.0.0")]
+  [EditorBrowsable(EditorBrowsableState.Advanced)]
+  [CompilerGenerated]
+  internal sealed class MySettings : ApplicationSettingsBase
+  {
+    private static MySettings defaultInstance = (MySettings) SettingsBase.Synchronized((SettingsBase) new MySettings());
+    private static bool addedHandler;
+    private static object addedHandlerLockObject = RuntimeHelpers.GetObjectValue(new object());
+
+    [DebuggerNonUserCode]
+    [EditorBrowsable(EditorBrowsableState.Advanced)]
+    private static void AutoSaveSettings(object sender, EventArgs e)
+    {
+      if (!MyProject.Application.SaveMySettingsOnExit)
+        return;
+      MySettingsProperty.Settings.Save();
+    }
+
+    public static MySettings Default
+    {
+      get
+      {
+        if (!MySettings.addedHandler)
+        {
+          object handlerLockObject = MySettings.addedHandlerLockObject;
+          ObjectFlowControl.CheckForSyncLockOnValueType(handlerLockObject);
+          bool lockTaken = false;
+          try
+          {
+            Monitor.Enter(handlerLockObject, ref lockTaken);
+            if (!MySettings.addedHandler)
+            {
+              MyProject.Application.Shutdown += (ShutdownEventHandler) ((sender, e) =>
+              {
+                if (!MyProject.Application.SaveMySettingsOnExit)
+                  return;
+                MySettingsProperty.Settings.Save();
+              });
+              MySettings.addedHandler = true;
+            }
+          }
+          finally
+          {
+            if (lockTaken)
+              Monitor.Exit(handlerLockObject);
+          }
+        }
+        return MySettings.defaultInstance;
+      }
+    }
+  }
+}
