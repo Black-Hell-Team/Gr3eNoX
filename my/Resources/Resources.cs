1ab33b6309af5cc777f3abdc5695257b0bf2ec03
Gr3n0xX <78820403+grenoxx@users.noreply.github.com>
2021-03-21 18:12:26 -0300

    Create Resources.cs

---
+++ my/Resources/Resources.cs
@@ -0,0 +1,43 @@
+//Type: Gr3eNoX_Exploit_Scanner_V80._0.My.MyProject
+// Assembly: Gr3eNoX Exploit Scanner V8.0, Version=8.0.0.0, Culture=neutral, PublicKeyToken=null
+
+using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.CompilerServices;
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.Resources;
+using System.Runtime.CompilerServices;
+
+namespace Gr3eNoX_Exploit_Scanner_V80._0.My.Resources
+{
+  [StandardModule]
+  [CompilerGenerated]
+  [HideModuleName]
+  [DebuggerNonUserCode]
+  [GeneratedCode("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+  internal sealed class Resources
+  {
+    private static ResourceManager resourceMan;
+    private static CultureInfo resourceCulture;
+
+    [EditorBrowsable(EditorBrowsableState.Advanced)]
+    internal static ResourceManager ResourceManager
+    {
+      get
+      {
+        if (object.ReferenceEquals((object) Gr3eNoX_Exploit_Scanner_V80._0.My.Resources.Resources.resourceMan, (object) null))
+          Gr3eNoX_Exploit_Scanner_V80._0.My.Resources.Resources.resourceMan = new ResourceManager("Gr3eNoX_Exploit_Scanner_V80._0.Resources", typeof (Gr3eNoX_Exploit_Scanner_V80._0.My.Resources.Resources).Assembly);
+        return Gr3eNoX_Exploit_Scanner_V80._0.My.Resources.Resources.resourceMan;
+      }
+    }
+
+    [EditorBrowsable(EditorBrowsableState.Advanced)]
+    internal static CultureInfo Culture
+    {
+      get => Gr3eNoX_Exploit_Scanner_V80._0.My.Resources.Resources.resourceCulture;
+      set => Gr3eNoX_Exploit_Scanner_V80._0.My.Resources.Resources.resourceCulture = value;
+    }
+  }
+}
